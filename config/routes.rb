1b819bc1a38dcaea633959d4b0ac9542c74351ad
Daniel Hahn <ghub@limitedcreativity.org>
2010-01-15 10:37:28 +0100

    generate hobo --add-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+
+  Hobo.add_routes(map)
+
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
