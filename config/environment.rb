0510f7dc3a8b0b3c581684a1afd2f319c99f48fd
Daniel Hahn <ghub@limitedcreativity.org>
2010-01-28 19:09:29 +0100

    Working Hobo Backend

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -33,7 +33,6 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.gem 'hobo'
   config.time_zone = 'UTC'
   config.logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}.log", 3, 104857600)
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,35 @@
 ActionController::Routing::Routes.draw do |map|
-
   map.site_search  'search', :controller => 'admin/front', :action => 'search'
   map.admin '/admin', :controller => 'admin/front', :action => 'index'
 
   Hobo.add_routes(map)
 
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
 
-  map.resource :session
+  # Autocomplete on URI
+  map.connect 'sources/auto_complete_for_uri', :controller => 'sources', :action => 'auto_complete_for_uri'
 
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
+  # Ontologies
+  map.resources :ontologies
 
-  map.resource :session
+  # Routes for the source data
+  map.connect 'source_data/:id', :controller => 'source_data',
+    :action => 'show'
 
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
+  map.connect 'source_data/:type/:location', :controller => 'source_data',
+    :action => 'show_tloc',
+    :requirements => { :location => /[^\/]+/ } # Force the location to match also filenames with points etc.
 
-  map.resource :session
+  map.resources :data_records, :controller => 'source_data'
+
+  # Routes for types
+  map.resources :types
+
+  # Routes for the sources
+  map.resources :sources, :requirements => { :id => /.+/  }
+
+  # Default semantic dispatch
+  map.connect ':dispatch_uri.:format', :controller => 'sources', :action => 'dispatch',
+    :requirements => { :dispatch_uri => /[^\.]+/ }
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -47,6 +49,12 @@ ActionController::Routing::Routes.draw do |map|
 
   # Sample resource route with sub-resources:
   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
+  
+  # Sample resource route with more complex sub-resources
+  #   map.resources :products do |products|
+  #     products.resources :comments
+  #     products.resources :sales, :collection => { :recent => :get }
+  #   end
 
   # Sample resource route within a namespace:
   #   map.namespace :admin do |admin|
@@ -54,67 +62,14 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  # See how all your routes lay out with "rake routes"
-  
-  # Default route
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+  # map.root :controller => "welcome"
 
-  map.namespace :admin do |admin|
-    admin.resources :translations, :collection => { :search => :get }
-    admin.resources :users, :active_scaffold => true
-    admin.resources :sources, :active_scaffold => true
-    admin.resources :locales
-    admin.resources :background, :active_scaffold => true
-    admin.resources :custom_templates, :active_scaffold => true
-    admin.resources :data_records, :active_scaffold => true
-  end
+  # See how all your routes lay out with "rake routes"
 
-  map.connect 'sources/auto_complete_for_uri', :controller => 'sources', :action => 'auto_complete_for_uri'
-
-  # Routes for login and users handling
-  map.resources :users
-  map.login  'login',  :controller => 'sessions', :action => 'create'
-  map.logout 'logout', :controller => 'sessions', :action => 'destroy'
-  map.admin  'admin',  :controller => 'admin',    :action => 'index'
-  map.open_id_complete 'session', :controller => 'sessions', :action => 'create', :requirements => { :method => :get }
-  map.resource :session
-  map.resources :languages, :member => { :change => :get }
-
-  # Routes for the ontologies
-  map.resources :ontologies
-  
-  # Routes for the sources
-  map.resources :sources, :requirements => { :id => /.+/  }
-  
-  # Routes for types
-  map.resources :types
-
-  # Routes for hyperedition previews
-  map.connect '/preview', :controller => 'preview'
-  
-  # Routes for the source data
-  map.connect 'source_data/:id', :controller => 'source_data',
-    :action => 'show'
-  map.connect 'source_data/:type/:location', :controller => 'source_data',
-    :action => 'show_tloc',
-    :requirements => { :location => /[^\/]+/ } # Force the location to match also filenames with points etc.
-
-  map.resources :data_records, :controller => 'source_data'
-  
-  # Routes for the widget engine
-  # map.resources :widgeon, :collection => { :callback => :all } do |widgets|
-  #   widgets.connect ':file', :controller => 'widgeon', :action => 'load_file', :requirements => { :file => %r([^;,?]+) }
-  # end
-    
-  # Routes for import
-  map.connect 'import/:action', :controller => 'import', :action => 'start_import'
-
-  # Default semantic dispatch
-  map.connect ':dispatch_uri.:format', :controller => 'sources', :action => 'dispatch',
-    :requirements => { :dispatch_uri => /[^\.]+/ }
-
-  # map.connect ':controller/:action/:id.:format'
+  # Install the default routes as the lowest priority.
+  # Note: These default routes make all actions in every controller accessible via GET requests. You should
+  # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
-  
-  
+  # map.connect ':controller/:action/:id.:format'
 end
