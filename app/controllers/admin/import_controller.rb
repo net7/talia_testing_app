6da136ecd75bfcb124d3a11d31df82d65fa8dbe9
Daniel Hahn <ghub@limitedcreativity.org>
2010-05-02 18:50:41 +0200

    Basic Import controller

--- app/controllers/admin/import_controller.rb
+++ app/controllers/admin/import_controller.rb
@@ -2,4 +2,11 @@ class Admin::ImportController < ApplicationController
   
   hobo_controller
   
+  def index
+  end
+  
+  def upload_import
+    TaliaCore::ActiveSource.create_from_xml(params[:import_file].read)
+  end
+  
 end
