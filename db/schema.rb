ce7eac2b262d0a8a4e6bfc159474354968d66f46
rik <rik@arch-11.localdomain>
2010-06-22 16:45:28 +0200

    * changed shebang for scripts; * deleted migration file for bj (not used anymore); * small fix to google map view: do not render if the source has no coordinates.

--- db/schema.rb
+++ db/schema.rb
@@ -20,52 +20,6 @@ ActiveRecord::Schema.define(:version => 20100118154130) do
 
   add_index "active_sources", ["uri"], :name => "index_active_sources_on_uri", :unique => true
 
-  create_table "bj_config", :primary_key => "bj_config_id", :force => true do |t|
-    t.text "hostname"
-    t.text "key"
-    t.text "value"
-    t.text "cast"
-  end
-
-  create_table "bj_job", :primary_key => "bj_job_id", :force => true do |t|
-    t.text     "command"
-    t.text     "state"
-    t.integer  "priority"
-    t.text     "tag"
-    t.integer  "is_restartable"
-    t.text     "submitter"
-    t.text     "runner"
-    t.integer  "pid"
-    t.datetime "submitted_at"
-    t.datetime "started_at"
-    t.datetime "finished_at"
-    t.text     "env"
-    t.text     "stdin"
-    t.text     "stdout"
-    t.text     "stderr"
-    t.integer  "exit_status"
-  end
-
-  create_table "bj_job_archive", :primary_key => "bj_job_archive_id", :force => true do |t|
-    t.text     "command"
-    t.text     "state"
-    t.integer  "priority"
-    t.text     "tag"
-    t.integer  "is_restartable"
-    t.text     "submitter"
-    t.text     "runner"
-    t.integer  "pid"
-    t.datetime "submitted_at"
-    t.datetime "started_at"
-    t.datetime "finished_at"
-    t.datetime "archived_at"
-    t.text     "env"
-    t.text     "stdin"
-    t.text     "stdout"
-    t.text     "stderr"
-    t.integer  "exit_status"
-  end
-
   create_table "custom_templates", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
