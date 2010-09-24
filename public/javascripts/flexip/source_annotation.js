62e31ef404aad8a512172ecbc079e9271b782801
rik <giomi@netseven.it>
2010-09-24 10:53:09 +0200

    Some fixes on Flexip for Swickynotes

--- public/javascripts/flexip/source_annotation.js
+++ public/javascripts/flexip/source_annotation.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
         url = $(this).attr("href");
         layers = [];
         selection = null;
-
         if(jthc && jthc.imageFragments[url])
-            for(fragment in jthc.imageFragments[url]) 
-                layers.push(JSON.parse($.base64.decode(fragment)));
+            for(fragment in jthc.imageFragments[url])
+                if(coordinates = jthc.imageFragments[url][fragment]["http://discovery-project.eu/ontologies/philoSpace/hasCoordinates"])
+                    layers.push(JSON.parse($.base64.decode(coordinates)));
 
         loadFlexip(url, layers, selection);
         return false;
