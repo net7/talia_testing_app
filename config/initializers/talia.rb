d1f6f1c393770d4369a1296b9c303c7aae1e062a
Danilo Giacomi <giacomi@netseven.it>
2010-09-27 14:54:52 +0200

    added png to the image types passed through iip

--- config/initializers/talia.rb
+++ config/initializers/talia.rb
@@ -7,6 +7,7 @@ TaliaCore::Initializer.run("talia_core")
 
 TaliaCore::DataTypes::MimeMapping.add_mapping(:jpeg, TaliaCore::DataTypes::ImageData, :create_iip)
 TaliaCore::DataTypes::MimeMapping.add_mapping(:tiff, TaliaCore::DataTypes::ImageData, :create_iip)
+TaliaCore::DataTypes::MimeMapping.add_mapping(:png, TaliaCore::DataTypes::ImageData, :create_iip)
 
 TaliaCore::SITE_NAME = TaliaCore::CONFIG['site_name'] || 'Discovery Source'
 
