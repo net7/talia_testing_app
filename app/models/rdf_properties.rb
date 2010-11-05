6fca092a3c211d40bfcaa635619a194ead449cc0
Danilo Giacomi <giacomi@netseven.it>
2010-11-05 16:27:50 +0100

    changed to use the static_data_prefix when showing images via flexip

--- app/models/rdf_properties.rb
+++ app/models/rdf_properties.rb
@@ -2,7 +2,10 @@ module RdfProperties
   
   def rdf_property(shortcut, property, options = {})
     options.to_options!
-    type = (options.delete(:type) || :string)
+    type = (options[:type] || :string)
+    unless type.is_a?(Class)
+      options.delete(:type)
+    end
     singular_property shortcut, property, options
     declare_attr_type shortcut, type
   end
