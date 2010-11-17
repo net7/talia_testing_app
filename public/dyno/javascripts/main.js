1f96eed7ea34a1d1576e273edc7867c613a890f1
rik <giomi@netseven.it>
2010-11-17 10:58:24 +0100

    * Added prototype javascript application that connects to a bridge   to sindice and creates a popup table with some info about one or more uris. * Connected application to demo via configuration value.

---
+++ public/dyno/javascripts/main.js
@@ -0,0 +1,130 @@
+$(function() {
+    var settings = {
+        uri: [],
+        source: 'http://localhost:8182/sindice',
+        lang: 'en',
+        altQuerystring: true,
+        jsonp: true
+    };
+
+    if($("#dyno_uris a[href]", window.opener.document)) {
+        $("#dyno_uris a[href]", window.opener.document).each(function(i, a) {
+            settings.uri.push($(a).attr("href"));
+        });
+
+        if($("section#dyno-attributes"))
+            $("section#dyno-attributes").dyno(settings);
+    }
+    return;
+});
+
+(function($) {
+    $.fn.dyno = function(settings) {
+        var row =null;
+        var values = {};
+
+        var config = {
+            uri: null,
+            values: null,
+            source: null,
+            lang: 'en'
+        };
+
+        function drawImage(url, text) {
+            var image = '<a href="'+url+'">';
+            image += '<img width="50" height="50" src="'+url+'" alt="'+text+'" title="'+text+'"/>';
+            image += '</a>';
+            return image;
+        };
+
+        function drawLink(url, text) {
+            return '<a href="'+url+'">'+text+'</a><br/>';
+        };
+
+        function drawText(text) {
+            return text+'<br/>';
+        };
+
+        function addRow(element, name, value) {
+            var temp = $(tableRow).clone();
+            $(".dyno-name", temp).html(name);
+            $(".dyno-value", temp).html(value);
+            temp.appendTo(element);
+        };
+
+        function draw(values, element) {
+            if(!values) return false;
+            tableRow = $(".dyno-attribute", element).first().detach();
+            if(!tableRow.html()) return false;
+
+            $("#dyno-title", element).text(values['title']);
+            for(var i in values.rows) {
+                var row = values.rows[i];
+                if(typeof row.value == 'string') row.value = [row.value];
+                if(typeof row.source == 'string') row.source = [row.source];
+                if(row.type == 'image') {
+                    var image = '';
+                    for(var j = 0; j < row.value.length; j++)
+                        image += drawImage(row.value[j].url, row.value[j].text);
+                    addRow(element, row.name, image);
+                }
+                else if(row.type == 'link') {
+                    var link = '';
+                    for(var j = 0; j < row.value.length; j++)
+                        link += drawLink(row.value[j].url, row.value[j].text);
+                    addRow(element, row.name, link);
+                }
+                else {
+                    var text = '';
+                    for(var j = 0; j < row.value.length; j++)
+                        text += drawText(row.value[j].text);
+                    addRow(element, row.name, text);
+                }
+            }
+            return true;
+        }
+
+        function urlEncode(url) {
+            return escape(url).replace(/\+/g,'%2B').replace(/%20/g, '+').replace(/\*/g, '%2A').replace(/\//g, '%2F').replace(/@/g, '%40')
+        }
+
+        function altQuerystring(uri) {
+            for(i in uri)
+                uri[i] = urlEncode(uri[i]);
+
+            return settings.source+"/"+uri.join(',')+"/"+urlEncode(settings.lang);
+        }
+
+        if(settings) $.extend(config, settings);
+
+        return this.each(function() {
+            if(config.values) return draw(config.values, $(this));
+
+            var uri = config.uri || $(this).attr("rdf:about");
+            if(!uri) return false;
+            if(typeof uri == 'string') uri = [uri];
+           
+
+            var request = (settings.altQuerystring) ? altQuerystring(uri) : settings.source;
+            var params  = (settings.altQuerystring) ? {} : {uri: uri.join(','), lang: config.lang};
+            var element = $(this);
+
+            $("#loading").show();
+
+            if(settings.jsonp) {
+                $.getJSON(request+"?callback=?", params, function(data) {
+                    /// TODO: error handling.
+                    draw(data, element);
+                    $("#loading").hide();
+                });
+            }
+            else {
+                $.get(request, params, function(data) {
+                    /// TODO: error handling.
+                    draw(data, element);
+                    $("#loading").hide();
+                });
+            }
+        });
+    };
+})(jQuery);
---
+++ public/javascripts/dyno.js
@@ -0,0 +1,22 @@
+$(function() {
+    var dyno_uris = [];
+    var dyno_sameAs = null;
+    if(dyno_sameAs = $(".predicate:contains('owl:sameAs')", "#properties_table")) {
+        var dyno_sameAsUris = $("span.value", dyno_sameAs.closest('td').next());
+        /// Confirm there is at least one sameAs link.
+        if(dyno_sameAsUris && $("a[href]", dyno_sameAsUris).length) {
+            /// Make the predicate span containing the "owl:sameAs" text
+            /// into a link which opens the dyno popup.
+            dyno_sameAs.html('<a href="#" id="dyno_popup">'+dyno_sameAs.text()+'</a>');
+            dyno_sameAsUris.attr("id", "dyno_uris");
+
+            $("#dyno_popup").live('click', function() {
+                dyno_window = window.open('/dyno/', 'dyno_window', 
+                                          "width=840,height=820,scrollbars=no,toolbar=no,location=no,status=no,directories=no,menubar=no");
+
+                dyno_window.focus();
+                return false;
+            });
+        }
+    }
+});
