e8a10cd73bd068d5af4d46a06746dd89506f9cc8
Danilo Giacomi <giacomi@netseven.it>
2010-12-03 17:20:27 +0100

    added rdf_property declaration to manuscript.rb model

--- app/models/manuscript.rb
+++ app/models/manuscript.rb
@@ -2,32 +2,34 @@ class Manuscript < Book
   hobo_model # Don't put anything above this
 
   include StandardPermissions
-  extend RdfProperty
+  extend RdfProperties
   has_rdf_type N::SCHOP.Manuscript
 
   after_create :set_title_from_uri
+
+  autofill_uri :force => true
 
   declare_attr_type :name, :string
   declare_attr_type :title, :string
 
   rdf_property :volume, N::DCT.isPartOf, :type => :string
-  singular_property :description, N::DCNS.description, :type => :string
+  rdf_property :description, N::DCNS.description, :type => :string
 
-  singular_property :date, N::DCT.date # this accept a range, though it's recommended to follow a standard (ISO861)
-  singular_property :width, N::SCHOP.width
-  singular_property :height, N::SCHOP.height
-  singular_property :binding_type, N::SCHOP.binding_type
-  singular_property :old_title, N::SCHOP.old_title
-  singular_property :new_title, N::SCHOP.new_title
-  singular_property :ex_libris, N::SCHOP.ex_libris
-  singular_property :sheets_number, N::SCHOP.sheets_number
-  singular_property :numbering_type, N::SCHOP.numbering_type
-  singular_property :numbering_author, N::SCHOP.numbering_author
-  singular_property :numbering_writing_type, N::SCHOP.numbering_writing_type
-  singular_property :notes, N::SCHOP.notes
-  singular_property :repository, N::SCHOP.repository
-  singular_property :repository_structure, N::SCHOP.repository_structure
-  singular_property :shelfmark, N::SCHOP.shelfmark
+  rdf_property :date, N::DCT.date, :type => :string # this accept a range, though it's recommended to follow a standard (ISO861)
+  rdf_property :width, N::SCHOP.width, :type => :string
+  rdf_property :height, N::SCHOP.height, :type => :string
+  rdf_property :binding_type, N::SCHOP.binding_type, :type => :string
+  rdf_property :old_title, N::SCHOP.old_title, :type => :string
+  rdf_property :new_title, N::SCHOP.new_title, :type => :string
+  rdf_property :ex_libris, N::SCHOP.ex_libris, :type => :string
+  rdf_property :sheets_number, N::SCHOP.sheets_number, :type => :string
+  rdf_property :numbering_type, N::SCHOP.numbering_type, :type => :string
+  rdf_property :numbering_author, N::SCHOP.numbering_author, :type => :string
+  rdf_property :numbering_writing_type, N::SCHOP.numbering_writing_type, :type => :string
+  rdf_property :notes, N::SCHOP.notes, :type => :string
+  rdf_property :repository, N::SCHOP.repository, :type => :string
+  rdf_property :repository_structure, N::SCHOP.repository_structu, :type => :string
+  rdf_property :shelfmark, N::SCHOP.shelfmark, :type => :string
 
 
   def add_index_element(position, title, start_page, end_page)
